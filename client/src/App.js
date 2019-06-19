64231ee84f72c9bd5f098cb6c43a86572cc5a0dd
Battut Alexandre <alexandre.battut@student-cs.fr>
2019-06-19 15:47:14 +0200

    Front films recommandés sans back

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import raid from './Raid.png'
+import Itsas from './IM.jpg'
+import aventuraid from './Aventuraid.jpg'
 function App() {
   return (
     <div className="App">
@@ -14,7 +16,37 @@ function App() {
       </header>
       <body>
         <div className="Recommandé">
+
           <h2>Recommandé pour vous</h2>
+
+          <div className="FilmsRecommandés">
+
+              <img src={logo} className="img_film1" alt="Image du film 1" />
+
+              <div className="Film1">
+
+                  <h3 className="TitreFilm1"> GugliMovie </h3>
+                  <p> Auteur: Filix </p>
+                  <p> Durée: 2h30 </p>
+                  <p> Genre: Horreur </p>
+
+              </div>
+
+              <img src={Itsas} className="img_film2" alt="Image du film 2" />
+
+              <div className="Film2">
+
+                  <h3 className="TitreFilm2"> Itsas Mendi </h3>
+                  <p> Auteur: RaidCS </p>
+                  <p> Durée: 5j </p>
+                  <p> Genre: Sport </p>
+              </div>
+
+              <img src={aventuraid} className="img_film3" alt="Image du film 3" />
+              <h3 className="TitreFilm3"> Aventuraid </h3>
+              <img src={aventuraid} className="img_film4" alt="Image du film 3" />
+              <h3 className="TitreFilm4"> Aventuraid </h3>
+          </div>
         </div>
       </body>
     </div>
