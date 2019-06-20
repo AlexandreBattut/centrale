1c5381b28e11278184aecb9835c15654ccb4a84e
Felix-GUGLIELMO <felix.guglielmo@student-cs.fr>
2019-06-20 10:04:02 +0200

    Mise en relation du Backend et du Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,38 @@ import green_book from './green_book.jpg'
 import forrest_gump from './forrest_gump.jpg'
 import ligne_verte from './ligne_verte.jpg'
 
-const [movie, setMovie] = React.useState(null);
-              React.useEffect(() => {
-                superagent
-                  .get("http://localhost:5000/application/movie/Forrest Gump")
-                  .then(response => setMovie(response.body.movie));
-              }, []);
 
-function App() {
+
+
+function App() {  
+  const superagent = require('superagent');
+  const [movie1, setMovie] = React.useState(0);
+  React.useEffect(() => {
+    superagent
+      .get("http://localhost:5000/application/movie/Forrest Gump")
+      .then(response => setMovie(response.body.movie));
+  }, []);
+  const [movie2, setMovie2] = React.useState(0);
+  React.useEffect(() => {
+    superagent
+      .get("http://localhost:5000/application/movie/La Ligne Verte")
+      .then(response => setMovie2(response.body.movie));
+  }, []);
+  const [movie3, setMovie3] = React.useState(0);
+  React.useEffect(() => {
+    superagent
+      .get("http://localhost:5000/application/movie/Bohemian Rhapsody")
+      .then(response => setMovie3(response.body.movie));
+  }, []);
+  const [movie4, setMovie4] = React.useState(0);
+  React.useEffect(() => {
+    superagent
+      .get("http://localhost:5000/application/movie/Green Book : Sur Les Routes Du Sud")
+      .then(response => setMovie4(response.body.movie));
+  }, []);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -27,40 +51,44 @@ function App() {
         <div className="Recommandé">
 
           <h2>Recommandé pour vous</h2>
-          <p> {movie.title} </p>
 
           <div className="FilmsRecommandés">
 
-              <img src={ligne_verte} className="img_film1" alt="Image du film 1" />
-
+              <img src={forrest_gump} className="img_film1" alt="Image du film 1" />
               <div className="Film1">
-
-                  <h3 className="TitreFilm1"> GugliMovie </h3>
-                  <p> Auteur: Filix </p>
-                  <p> Durée: 2h30 </p>
-                  <p> Genre: Horreur </p>
-
+                  <h3 className="TitreFilm1"> {movie1.title} </h3>
+                  <p> Auteur: {movie1.author} </p>
+                  <p> Durée: {movie1.length} </p>
+                  <p> Genre: {movie1.genre} </p>
+                  <p> note: {movie1.note}/5 </p>
               </div>
 
-              
-
-
-
-
-              <img src={green_book} className="img_film2" alt="Image du film 2" />
-
+              <img src={ligne_verte} className="img_film2" alt="Image du film 2" />
               <div className="Film2">
-
-                  <h3 className="TitreFilm2"> Itsas Mendi </h3>
-                  <p> Auteur: RaidCS </p>
-                  <p> Durée: 5j </p>
-                  <p> Genre: Sport </p>
+                  <h3 className="TitreFilm2"> {movie2.title} </h3>
+                  <p> Auteur: {movie2.author} </p>
+                  <p> Durée: {movie2.length} </p>
+                  <p> Genre: {movie2.genre} </p>
+                  <p> note: {movie2.note}/5 </p>
               </div>
 
-              <img src={forrest_gump} className="img_film3" alt="Image du film 3" />
-              <h3 className="TitreFilm3"> Aventuraid </h3>
-              <img src={bohemian_rhapsody} className="img_film4" alt="Image du film 3" />
-              <h3 className="TitreFilm4"> Aventuraid </h3>
+              <img src={bohemian_rhapsody} className="img_film3" alt="Image du film 3" />
+              <div className="Film3">
+                  <h3 className="TitreFilm3"> {movie3.title} </h3>
+                  <p> Auteur: {movie3.author} </p>
+                  <p> Durée: {movie3.length} </p>
+                  <p> Genre: {movie3.genre} </p>
+                  <p> note: {movie3.note}/5 </p>
+              </div>
+
+              <img src={green_book} className="img_film4" alt="Image du film 4" />
+              <div className="Film4">
+                  <h3 className="TitreFilm4"> {movie4.title} </h3>
+                  <p> Auteur: {movie4.author} </p>
+                  <p> Durée: {movie4.length} </p>
+                  <p> Genre: {movie4.genre} </p>
+                  <p> note: {movie4.note}/5 </p>
+              </div>
           </div>
         </div>
       </body>
